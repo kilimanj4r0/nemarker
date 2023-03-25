f0b55d05db651cd6fba02c21e7c687d96ff4b2ef
Artem Batalov <a@batalov.me>
2023-03-25 13:43:39 +0300

    version

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -5,7 +5,6 @@ import {Input, TextArea} from "../components/form/styled";
 const MainPage: React.FC = () => {
     return (
         <PageWrapper>
-            <h1>Main path page</h1>
             <div>
                 <Input placeholder="что сделать?"></Input>
                 <br /><br /><br />
