ef4d105c4cc7f7f7968231327b24327c352442e1
Artem Batalov <a@batalov.me>
2023-04-29 04:30:35 +0300

    about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import {Block, Description, Profile, ProfilePhoto, ServiceDescriptionBlock, Wrapper} from "./styled";
 import mock from '../../assets/logo-small.svg';
+import artem from '../../assets/images/photos/artem-batalov.png';
+import vova from '../../assets/images/photos/vladimir-makharev.png';
+import liza from '../../assets/images/photos/elizaveta-semenova.png';
+import kamil from '../../assets/images/photos/kamil-foatov.png';
 
 type TeamType = {
     photoSrc: string;
@@ -10,24 +14,24 @@ type TeamType = {
 
 const team: TeamType[] = [
     {
-        photoSrc: mock,
+        photoSrc: vova,
         name: 'Владимир Махарев',
-        description: 'Lorem ipsum dolor sit amet consectetur. Cras eu orci et adipiscing cursus.'
+        description: 'Тимлид, большой фронтендер и хороший человек'
     },
     {
-        photoSrc: mock,
+        photoSrc: artem,
         name: 'Артем Баталов',
-        description: 'Lorem ipsum dolor sit amet consectetur. Cras eu orci et adipiscing cursus.'
+        description: 'Фулл-стэк, генератор идей'
     },
     {
-        photoSrc: mock,
+        photoSrc: kamil,
         name: 'Камиль Фоатов',
-        description: 'Lorem ipsum dolor sit amet consectetur. Cras eu orci et adipiscing cursus.'
+        description: 'Дизайнер, стендапер, получатель сетевого онанизма'
     },
     {
-        photoSrc: mock,
+        photoSrc: liza,
         name: 'Елизавета Семенова',
-        description: 'Lorem ipsum dolor sit amet consectetur. Cras eu orci et adipiscing cursus.'
+        description: 'Фронтендер по менбше, писатель тестов'
     },
 ];
 
